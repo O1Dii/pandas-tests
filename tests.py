f27478f47ef2fd4d9f015fe0f7b8b3b641bc022d
O1Dii <oldi730051@mail.ru>
2020-06-19 13:37:38 +0300

    Added hypothesis to unittest

--- tests.py
+++ tests.py
@@ -1,37 +1,25 @@
 import unittest
 from unittest.mock import Mock, patch
 
-import pandas as pd
-from numpy import nan
+from hypothesis import given
+from hypothesis.extra.pandas import column, data_frames
 
 from main import sum
 
 
 class TestMain(unittest.TestCase):
+    @given(df=data_frames([
+        column('1', dtype='float'),
+        column('2', dtype='float')
+    ]))
     @patch("pandas.read_csv")
-    def test_sum_basic(self, read_csv_mock: Mock):
-        read_csv_mock.return_value = pd.DataFrame({'1': [1, 2, 3], '2': [3, 2, 1]})
+    def test_sum_basic(self, read_csv_mock: Mock, df):
+        read_csv_mock.return_value = df
         results = sum()
         read_csv_mock.assert_called_once()
 
-        pd.testing.assert_frame_equal(results, pd.DataFrame({'1': [1, 2, 3], '2': [3, 2, 1], '3': [4, 4, 4]}))
-
-    @patch("pandas.read_csv")
-    def test_sum_with_nan(self, read_csv_mock: Mock):
-        read_csv_mock.return_value = pd.DataFrame({'1': [nan, nan, 3], '2': [3, nan, 1]})
-        results = sum()
-        read_csv_mock.assert_called_once()
-
-        pd.testing.assert_frame_equal(results, pd.DataFrame({'1': [nan, nan, 3], '2': [3, nan, 1], '3': [nan, nan, 4]}))
-
-    @patch("pandas.read_csv")
-    def test_sum_with_strings(self, read_csv_mock: Mock):
-        read_csv_mock.return_value = pd.DataFrame({'1': ['a', 2, 3], '2': [3, 2, 1]})
-
-        with self.assertRaises(TypeError):
-            sum()
-
-        read_csv_mock.assert_called_once()
+        self.assertEqual(len(results.columns), 3)
+        self.assertEqual(results['3'].dtype, 'float')
 
 
 if __name__ == '__main__':
